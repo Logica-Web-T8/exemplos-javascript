dda6d98a7f3988ad74567e8eb317dff6391bab28
Ariel Junior <act117@einstein.br>
2023-01-18 20:58:05 -0300

    arquivo de variáveis

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+// comentário de uma linha 
+/* 
+  comentário para mais de 1 linha
+*/
+
+// ---------------------------------------------------------------------
+/*
+    declarando variáveis com var, let e const
+*/
+var nome_completo = "joão da silva"; //variavel pode ser alterada
+let idade = 20; //variavel pode ser alterada
+const cpf = "123456789"; //variavel NÃO pode ser alterada
+
+// console.log serve para "printar/mostrar" o valor no console do navegador
+console.log(nome_completo, idade, cpf)
+
+// alterar o valor das variáveis
+nome_completo = "Maria";
+idade = 25;
+
+console.log(nome_completo, idade, cpf)
