9b90a09c83acc8c45a3fd0d51e93ea89caba5e34
Ariel Junior <act117@einstein.br>
2023-01-19 20:16:05 -0300

    parte 1: variáveis finalizado

--- variaveis.js
+++ variaveis.js
@@ -19,4 +19,63 @@ console.log(nome_completo, idade, cpf)
 nome_completo = "Maria";
 idade = 25;
 
-console.log(nome_completo, idade, cpf)
+console.log(nome_completo, idade, cpf);
+
+//-------------------------------------------------------------------------------
+
+// Formas de declarar variáveis do tipo string
+var nome = "josé";
+var nome2 = 'maria';
+var nome3 = `joão`;
+
+// Formas de declarar váriaveis do tipo number
+let numero = 20; //inteiro
+let numeroDecimal = 15.5; //decimal
+
+// Declaração de variável tipo boolean (true / false)
+let bolean = false;
+
+// Declaração de arrays (listas)
+let frutas = ["maçã", "uva", "Melancia"];
+let numerosDaMegaSena = [12,15,30,20];
+let lista = ["teste", 12, true, 15.5];
+
+// Declaração de um objeto 
+let casa = {
+  "endereco": "Rio de Janeiro - Brasil",
+  'quantidade_banheiros': 2,
+  cor: "branca",
+  aluado: false
+};
+
+let experienciaProfissional = {
+  cargo: "Desenvolvedor FullStack",
+  nome_empresa: "Serasa",
+  data_inicio: "12/2022",
+  data_fim: null,
+  descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing",
+  atividades: ["atividade 1", "atividade 2"]
+};
+
+//Declaração de um array de objetos
+let experiencias = [
+  {
+    cargo: "Desenvolvedor FullStack",
+    nome_empresa: "Serasa",
+    data_inicio: "12/2022",
+    data_fim: null,
+    descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing",
+    atividades: ["atividade 1", "atividade 2"],
+  },
+  {
+    cargo: "Desenvolvedor Frontend",
+    nome_empresa: "Wise hands",
+    data_inicio: "05/2020",
+    data_fim: "11/2022",
+    descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing",
+    atividades: ["atividade 1", "atividade 2"]
+  }
+];
+
+//typeof função do javascript para saber o tipo da variável;
+console.log(typeof experiencias)
